d3ae6e12bbcfc59e1b0b0a0cd3ad609f45aca39d
hieu.nguyen <hieu.nguyen@metanet.co.kr>
2020-11-17 18:47:43 +0700

    Show offline message

--- js/main.js
+++ js/main.js
@@ -2,21 +2,52 @@
  *  It first checks whether the browser supports service workers. */
 
 if ('serviceWorker' in navigator) {
-    console.log('[MAIN] Service Worker is Supported!');
+  console.log('[MAIN] Service Worker is Supported!');
 
-    window.addEventListener('load', function () {
-        /** Register a service worker hosted
-         * at the root of the site using the default scope. */
-        navigator.serviceWorker.register('./service-worker.js')
-            // Return a Promise
-            .then(function (registration ) {
-                console.log('[MAIN] Service Worker registration succeeded:', registration);
-            })
-            .catch(function (err) {
-                console.error('[MAIN] Service Worker registration failed:', err);
-            })
-    });
+  window.addEventListener('load', function () {
+    /** Register a service worker hosted
+     * at the root of the site using the default scope. */
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      // Return a Promise
+      .then(function (registration) {
+        console.log(
+          '[MAIN] Service Worker registration succeeded:',
+          registration
+        );
+      })
+      .catch(function (err) {
+        console.error('[MAIN] Service Worker registration failed:', err);
+      });
+  });
+} else {
+  console.log('[MAIN] Service workers are not supported.');
 }
-else {
-    console.log('[MAIN] Service workers are not supported.');
+
+function isOnline() {
+  document.getElementById('online-status').innerHTML = '';
 }
+
+function isOffline() {
+  document.getElementById('online-status').innerHTML = "You're Offline!";
+}
+
+(function () {
+  if (window.addEventListener) {
+    /*
+        Works well in Firefox and Opera with the 
+        Work Offline option in the File menu.
+        Pulling the ethernet cable doesn't seem to trigger it.
+        Later Google Chrome and Safari seem to trigger it well
+    */
+    window.addEventListener('online', isOnline, false);
+    window.addEventListener('offline', isOffline, false);
+  } else {
+    /*
+        Works in IE with the Work Offline option in the 
+        File menu and pulling the ethernet cable
+    */
+    document.body.ononline = isOnline;
+    document.body.onoffline = isOffline;
+  }
+})();
--- service-worker.js
+++ service-worker.js
@@ -7,92 +7,94 @@ const CACHE_NAME = 'v1';
 //     "/css/style.css",
 // ];
 
-self.addEventListener("install", onInstall);
-self.addEventListener("activate", onActivate);
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
 // self.addEventListener("message", onMessage);
-self.addEventListener("fetch", onFetch);
+self.addEventListener('fetch', onFetch);
 
 /**
  * Install Event
- * @param {*} event 
+ * @param {*} event
  */
 function onInstall(event) {
-    console.log('WORKER: install event in progress.');
+  console.log('WORKER: install event in progress.');
 
-    // event.waitUntil(
-    //     /* The caches built-in is a promise-based API that helps you cache responses,
-    //         as well as finding and deleting them.
-    //     */
-    //     caches
-    //         /* You can open a cache by name, and this method returns a promise. We use
-    //             a versioned cache name here so that we can remove old cache entries in
-    //             one fell swoop later, when phasing out an older service work9er.
-    //         */
-    //         .open(CACHE_NAME)
-    //         .then(function (cache) {
-    //             /* After the cache is opened, we can fill it with the offline fundamentals.
-    //                The method below will add all resources we've indicated to the cache,
-    //                after making HTTP requests for each of them.
-    //             */
-    //             console.log('Service Worker Caching Files!');
-    //             return cache.addAll(CACHE_ASSETS);
-    //         })
-    //         .then(function () {
-    //             console.log('WORKER: install completed');
-    //             return self.skipWaiting();
-    //         })
-    // );
+  // event.waitUntil(
+  //     /* The caches built-in is a promise-based API that helps you cache responses,
+  //         as well as finding and deleting them.
+  //     */
+  //     caches
+  //         /* You can open a cache by name, and this method returns a promise. We use
+  //             a versioned cache name here so that we can remove old cache entries in
+  //             one fell swoop later, when phasing out an older service work9er.
+  //         */
+  //         .open(CACHE_NAME)
+  //         .then(function (cache) {
+  //             /* After the cache is opened, we can fill it with the offline fundamentals.
+  //                The method below will add all resources we've indicated to the cache,
+  //                after making HTTP requests for each of them.
+  //             */
+  //             console.log('Service Worker Caching Files!');
+  //             return cache.addAll(CACHE_ASSETS);
+  //         })
+  //         .then(function () {
+  //             console.log('WORKER: install completed');
+  //             return self.skipWaiting();
+  //         })
+  // );
 }
 
 /**
  * Activate Event
- * @param {*} event 
+ * @param {*} event
  */
 function onActivate(event) {
-    console.log('Service Worker Activated!');
-    // Remove unwanted caches
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cache) {
-                    if (cache !== CACHE_NAME) {
-                        console.log('Service Worker Clearing Old Cache!');
-                        return caches.delete(cache);
-                    }
-                })
-            )
+  console.log('Service Worker Activated!');
+  // Remove unwanted caches
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cache) {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker Clearing Old Cache!');
+            return caches.delete(cache);
+          }
         })
-    )
+      );
+    })
+  );
 }
 
 function onMessage({ data }) {
-    // 
+  //
 }
 
 function onFetch(event) {
-    console.log('[SERVICE WORKER] fetch event in progress.');
+  console.log('[SERVICE WORKER] fetch event in progress.');
 
-    event.respondWith(
-        fetch(event.request)
-            .then(function (response) {
-                // Make a copy of response
-                const resClone = response.clone();
+  if (event.request.mode === 'navigate') {
+    return event.respondWith(
+      fetch(event.request)
+        .then(function (response) {
+          // Make a copy of response
+          const resClone = response.clone();
 
-                // Open cache
-                caches
-                    .open(CACHE_NAME)
-                    .then(function (cache) {
-                        // Add response to cache
-                        cache.put(event.request, resClone);
-                    });
+          // Open cache
+          caches.open(CACHE_NAME).then(function (cache) {
+            // Add response to cache
+            cache.put(event.request, resClone);
+          });
 
-                return response;
-            })    
-            .catch(function () {
-                return caches.match(event.request);
-            })
-            .then(function (response) {
-                return response;
-            })
+          return response;
+        })
+        .catch(function () {
+          return caches.match(event.request);
+        })
+        .then(function (response) {
+          return response;
+        })
     );
+  } else {
+    console.log('Offline');
+  }
 }
